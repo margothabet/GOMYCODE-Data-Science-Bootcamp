94a07fc5699c9c65e1e001ebb079525ba00fa531
margothabet <132564582+margothabet@users.noreply.github.com>
2023-08-10 10:06:20 +0300

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pickle
+with open("model_pickle.pkl",'rb') as file:
+    ourModel = pickle.load(file)
+st.header("IRIS Project")
+Sepal_length=st.number_input("Enter Sepal length")
+Sepal_Width=st.number_input("Enter Sepal width")
+Petal_length=st.number_input("Enter Petal length")
+Petal_width=st.number_input("Enter Petal Width")
+
+# Create a dictionary to map class numbers to flower names
+class_to_flower = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+
+if st.button("Submit"):
+   predict=ourModel.predict([[Sepal_length,Sepal_Width,Petal_length,Petal_width]])
+   st.success(class_to_flower[predict[0]])
